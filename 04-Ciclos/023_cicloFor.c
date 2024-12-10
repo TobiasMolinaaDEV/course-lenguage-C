70d2ebb0e89655f187e9fff39608c8ee467a0008
Molina Tobias 禅 <tobiasmolinadev@gmail.com>
2024-12-10 00:07:19 -0300

    2nd commit course lenguage C

--- 04-Ciclos/023_cicloFor.c
+++ 04-Ciclos/023_cicloFor.c
@@ -2,6 +2,15 @@
 
 int main()
 {
+    // ciclo for 
+    // imprimir 1..5
+    int Repetidor = 5;
+
+    for (int contador = 1; contador <= Repetidor; contador++)
+    {
+        printf ("%d\n", contador);    
+
+    }
     
     return 0;
 }
